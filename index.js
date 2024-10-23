bb97d40e11fe0bd08a5a6a928e701721b55768cb
tarug345 <atrothunder.laflame345@gmail.com>
2024-10-23 15:44:04 -0300

    tentei

--- index.js
+++ index.js
@@ -13,37 +13,37 @@ const cartoes = [
         nome: 'Cartão 1',
         valor: '19,20',
         descricao: 'Descrição do Cartão 1',
-        imagem: 'https://driftforu.com/cdn/shop/products/image_0d0ed780-67c6-454e-8225-d01518819aa0.jpg?v=1710955863&width=1445'
+        img: 'https://driftforu.com/cdn/shop/products/image_0d0ed780-67c6-454e-8225-d01518819aa0.jpg?v=1710955863&width=1445'
     },
     {
         nome: 'Cartão 2',
         valor: '29,99',
         descricao: 'Descrição do Cartão 2',
-        imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrLJ6IKmikTJtwQBlCDbGxbtq5jkV_bdl8DQ&s'
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrLJ6IKmikTJtwQBlCDbGxbtq5jkV_bdl8DQ&s'
     },
     {
         nome: 'Cartão 3',
         valor: '39,50',
         descricao: 'Descrição do Cartão 3',
-        imagem: 'https://f.i.uol.com.br/fotografia/2020/02/10/15813740235e41da475cca0_1581374023_3x2_md.jpg'
+        img: 'https://f.i.uol.com.br/fotografia/2020/02/10/15813740235e41da475cca0_1581374023_3x2_md.jpg'
     },
     {
         nome: 'Cartão 4',
         valor: '55,55',
         descricao: 'Descrição do Cartão 4',
-        imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXlikNbPxUbc5Bfgt2XKylnWVQ5BeKaVYY0A&s'
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXlikNbPxUbc5Bfgt2XKylnWVQ5BeKaVYY0A&s'
     },
     {
         nome: 'Cartão 5',
         valor: '2,99',
         descricao: 'Descrição do Cartão 5',
-        imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD7hykkOSbTt9XA1_D1J8_kU9kcSMwYXdnIQ&s'
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD7hykkOSbTt9XA1_D1J8_kU9kcSMwYXdnIQ&s'
     },
     {
         nome: 'Cartão 6',
         valor: '12,33',
         descricao: 'Descrição do Cartão 6',
-        imagem: 'https://portal6.com.br/wp-content/uploads/2021/08/zeca.png'
+        img: 'https://portal6.com.br/wp-content/uploads/2021/08/zeca.png'
     }
 ];
 // Modificação na rota raiz para retornar o vetor de cartões

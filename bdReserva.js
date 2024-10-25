c21e18288b5c356b2a40692a9d5dff9ed6e23b3f
tarug345 <atrothunder.laflame345@gmail.com>
2024-10-25 17:13:38 -0300

    a

---
+++ bdReserva.js
@@ -0,0 +1,38 @@
+// const cartoes = [
+//     {
+//         nome: 'Silvia',
+//         valor: '19,20',
+//         descricao: 'Descrição do Cartão 1',
+//         img: 'https://driftforu.com/cdn/shop/products/image_0d0ed780-67c6-454e-8225-d01518819aa0.jpg?v=1710955863&width=1445'
+//     },
+//     {
+//         nome: 'Messi careca',
+//         valor: '29,99',
+//         descricao: 'Descrição do Cartão 2',
+//         img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrLJ6IKmikTJtwQBlCDbGxbtq5jkV_bdl8DQ&s'
+//     },
+//     {
+//         nome: 'Neymar',
+//         valor: '39,50',
+//         descricao: 'Descrição do Cartão 3',
+//         img: 'https://f.i.uol.com.br/fotografia/2020/02/10/15813740235e41da475cca0_1581374023_3x2_md.jpg'
+//     },
+//     {
+//         nome: 'O vasco vai me matar',
+//         valor: '55,55',
+//         descricao: 'Descrição do Cartão 4',
+//         img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXlikNbPxUbc5Bfgt2XKylnWVQ5BeKaVYY0A&s'
+//     },
+//     {
+//         nome: 'Ribamar',
+//         valor: '2,99',
+//         descricao: 'Descrição do Cartão 5',
+//         img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD7hykkOSbTt9XA1_D1J8_kU9kcSMwYXdnIQ&s'
+//     },
+//     {
+//         nome: 'Chico Buarque',
+//         valor: '12,33',
+//         descricao: 'Descrição do Cartão 6',
+//         img: 'https://portal6.com.br/wp-content/uploads/2021/08/zeca.png'
+//     }
+// ];
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const cors = require('cors');
 const express = require('express');
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./chaveFirebase.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+
+const db = admin.firestore();
 
 const app = express();
 const porta = 3000;
@@ -8,50 +17,61 @@ app.use(cors());
 app.use(express.json());
 
 // Vetor de objetos com os cartões no backend
-const cartoes = [
-    {
-        nome: 'Silvia',
-        valor: '19,20',
-        descricao: 'Descrição do Cartão 1',
-        img: 'https://driftforu.com/cdn/shop/products/image_0d0ed780-67c6-454e-8225-d01518819aa0.jpg?v=1710955863&width=1445'
-    },
-    {
-        nome: 'Messi careca',
-        valor: '29,99',
-        descricao: 'Descrição do Cartão 2',
-        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrLJ6IKmikTJtwQBlCDbGxbtq5jkV_bdl8DQ&s'
-    },
-    {
-        nome: 'Neymar',
-        valor: '39,50',
-        descricao: 'Descrição do Cartão 3',
-        img: 'https://f.i.uol.com.br/fotografia/2020/02/10/15813740235e41da475cca0_1581374023_3x2_md.jpg'
-    },
-    {
-        nome: 'O vasco vai me matar',
-        valor: '55,55',
-        descricao: 'Descrição do Cartão 4',
-        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXlikNbPxUbc5Bfgt2XKylnWVQ5BeKaVYY0A&s'
-    },
-    {
-        nome: 'Ribamar',
-        valor: '2,99',
-        descricao: 'Descrição do Cartão 5',
-        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD7hykkOSbTt9XA1_D1J8_kU9kcSMwYXdnIQ&s'
-    },
-    {
-        nome: 'Chico Buarque',
-        valor: '12,33',
-        descricao: 'Descrição do Cartão 6',
-        img: 'https://portal6.com.br/wp-content/uploads/2021/08/zeca.png'
-    }
-];
+// const cartoes = [
+//     {
+//         nome: 'Silvia',
+//         valor: '19,20',
+//         descricao: 'Descrição do Cartão 1',
+//         img: 'https://driftforu.com/cdn/shop/products/image_0d0ed780-67c6-454e-8225-d01518819aa0.jpg?v=1710955863&width=1445'
+//     },
+//     {
+//         nome: 'Messi careca',
+//         valor: '29,99',
+//         descricao: 'Descrição do Cartão 2',
+//         img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrLJ6IKmikTJtwQBlCDbGxbtq5jkV_bdl8DQ&s'
+//     },
+//     {
+//         nome: 'Neymar',
+//         valor: '39,50',
+//         descricao: 'Descrição do Cartão 3',
+//         img: 'https://f.i.uol.com.br/fotografia/2020/02/10/15813740235e41da475cca0_1581374023_3x2_md.jpg'
+//     },
+//     {
+//         nome: 'O vasco vai me matar',
+//         valor: '55,55',
+//         descricao: 'Descrição do Cartão 4',
+//         img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXlikNbPxUbc5Bfgt2XKylnWVQ5BeKaVYY0A&s'
+//     },
+//     {
+//         nome: 'Ribamar',
+//         valor: '2,99',
+//         descricao: 'Descrição do Cartão 5',
+//         img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD7hykkOSbTt9XA1_D1J8_kU9kcSMwYXdnIQ&s'
+//     },
+//     {
+//         nome: 'Chico Buarque',
+//         valor: '12,33',
+//         descricao: 'Descrição do Cartão 6',
+//         img: 'https://portal6.com.br/wp-content/uploads/2021/08/zeca.png'
+//     }
+// ];
 // Modificação na rota raiz para retornar o vetor de cartões
 
 // Rota específica para obter os cartões (opcional, pois agora também está na rota '/')
-app.get('/cartoes', (req, res) => {
-    res.status(200).json({ cartoes }); // Retorna o vetor de cartões
-    console.log('Cartões enviados');
+app.get('/cartoes', async (req, res) => {
+    try{
+        const response = await db.collection('cartoes').get();
+        const cartoes = response.docs.map(doc => doc.data({
+            id: doc.id, ...doc.data()
+        }));
+            console.log(cartoes);
+            res.status(200).json({cartoes}); // Retorna o vetor de cartões
+        console.log('Cartões enviados'); // Mensagem de sucesso
+    } catch (e){
+        console.log(e);
+        res.status(500).json({ error: 'erro' + e });
+        console.log('Erro' + e);
+    }
 });
 // Rota para criar um novo cartão
 app.post('/cartoes', (req, res) => { //antes era "/falas"
